29d7aa0b389c65d9bbc3df2a1f34c2914c8636ca
Noe7733 <noe777guillenwigetta@gmail.com>
2022-11-08 04:20:09 -0600

    Poblado del repo

---
+++ src/main/java/com/example/demo/ProyectoUnachOtifyApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoUnachOtifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoUnachOtifyApplication.class, args);
+	}
+
+}
